3308f1d214cfcab7b4463dcda6545619b018d04a
Natalie <NatalieAnna@users.noreply.github.com>
2020-12-01 11:42:43 +1100

    Add files via upload

---
+++ InvokingSuperclass/Main.java
@@ -0,0 +1,29 @@
+/* INVOKNG SUPERCLASS */
+
+// superclass
+class Animals {
+	public String makeNoise() {
+		return "I make noise - ";
+	}
+}
+
+// subclass
+class Canine extends Animals {
+	@Override
+	public String makeNoise() {
+		return super.makeNoise() + " Woof";
+	}
+}
+
+// subclass
+class Dog extends Canine {}
+
+// main method
+class Main {
+	public static void main(String[] args) {
+		Dog aDog = new Dog();
+		System.out.println(aDog.makeNoise());
+	}
+}
+
+/* prints "I make noise -  Woof" */
