3d9303405c931260b79fe4834c4207341c47c556
Natalie <NatalieAnna@users.noreply.github.com>
2020-12-04 12:01:46 +1100

    Add files via upload
    Refactored.

--- ObjectClass/Main.java
+++ ObjectClass/Main.java
@@ -1,107 +1,57 @@
-// superclass
 class Animals {
 	String location = "nowhere";
-	public boolean attendedSchool = false;
-
-	public Animals () {
-	}
 
 	public String makeNoise() {
 		return "none";
 	}
 
-        public boolean getSchool() {
-                return attendedSchool;
-        }
-
-	// all calls to this method return the Animals version
 	public String getLocation() {
 		return location;
 	}
 }
 
-// subclass
+
 class Canine extends Animals {
 	String location = "Bushtrack";
-	boolean attendedSchool = true;
 
 	@Override
-	public boolean getSchool() {
-		return attendedSchool;
-	}
-
-	public int canineOnlyMethod(int n) {
-		return n;
-	}
-
-	@Override // requires same name and param as parent class; must pass Canine IS-A Animal
 	public String makeNoise() {
 		return "Woof";
 	}
 
-        public String getLocation() {
-                return location;
-        }
+	// if commented out then inherits superclass "nowhere"
+	// public String getLocation() {
+	// 	return location;
+	// }
 }
 
-// subclass
+
 class Feline extends Animals {
 	String location = "Backyard";
-	boolean attendedSchool = false;
 
 	@Override
 	public String makeNoise() {
 		return "Meow";
 	}
 
-        public boolean getSchool() {
-                return attendedSchool;
-        }
-
-        public String getLocation() {
-                return location;
-        }
+	public String getLocation() {
+		return location;
+	}
 }
 
 class Tiger extends Feline {
 	String location = "Jungle";
-	boolean attendedSchool = true;
 
 	@Override
 	public String makeNoise() {
 		return "Roar";
 	}
 
-	// must have own getSchool()
-        public boolean getSchool() {
-                return attendedSchool;
-        }
-
-        public String getLocation() {
-                return location;
-        }
+	public String getLocation() {
+		return location;
+	}
 }
 
-// polymorphic arguments & return types
-class Vet {
-	public static String giveShot(Animals a) {
-		return a.makeNoise();
-	}
-
-	//Overload (not Override)
-	public static String giveShot(Animals a, int quantity) {
-		return a.makeNoise() + ", " + quantity;
-	}
-
-        //Overload (not Override)
-	// each subclass must have own getLocation()
-        public static String giveShotWhere(Animals a, int quantity) {
-                return a.makeNoise() + ", at " + a.getLocation() + ", " + quantity + " mLs.";
-        }
-
-}
-
-// main method
 class Main {
 	public static void main(String[] args) {
 		Animals[] animals = new Animals[3];
@@ -118,7 +68,8 @@ class Main {
 		System.out.println(animals[2].hashCode());
 		System.out.println(animals[1].toString());
 
-		for ( int i = 0 ; i <= animals.length ; i++ ) {
+		for ( int i = 0 ; i < animals.length ; i++ ) {
+			System.out.println(animals[i] + " is at " + animals[i].getLocation());
 		}
 	}
 }
@@ -130,5 +81,8 @@ The objects are not equal
 class Tiger
 1259475182
 Feline@4d7e1886
+Canine@452b3a41 is at nowhere
+Feline@4d7e1886 is at Backyard
+Tiger@4b1210ee is at Jungle
 
 */
