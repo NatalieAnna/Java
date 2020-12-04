d5cb858d6986437a16f806954aacb53b12ce9a94
Natalie <NatalieAnna@users.noreply.github.com>
2020-12-04 11:49:47 +1100

    Update Main.java
    Refactored.

--- AbstractConcrete/Main.java
+++ AbstractConcrete/Main.java
@@ -1,65 +1,79 @@
-// superclass
 abstract class Animals {
-        public abstract String makeNoise();
-	// throws error: Canine is not abstract and does not override abstract method sleep() in Animals
-	// solved by adding sleep() to class Canine.
-	public abstract void sleep();
+
+	public abstract String makeNoise();
+	public abstract String sleep();
 }
 
-// subclass
 class Canine extends Animals {
 
-	@Override // requires same name and param as parent class; must pass Canine IS-A Animal
+	// must override abstract makeNoise() in superclass
+	@Override
 	public String makeNoise() {
 		return "Woof";
 	}
 
-	// after this, thows error in class Feline
-	// error: Feline is not abstract and does not override abstract method sleep() in Animals
-	public void sleep() {};
+	public String sleep() {
+		return "zzz";
+	};
 }
 
-// subclass
 class Feline extends Animals {
 
+	// must override abstract makeNoise() in superclass
 	@Override
 	public String makeNoise() {
 		return "Meow";
 	}
 
-	// error solved. No additional error for class Tiger as is subclass of Feline.
-	public void sleep() {};
+	public String sleep() {
+		return "purr";
+	};
 }
 
-// subclass
 class Tiger extends Feline {
 
-	@Override
-	public String makeNoise() {
-		return "Roar";
-	}
+	// does not need to override makeNoise() in superclass. Returns "meow".
+	// @Override
+	// public String makeNoise() {
+	// 	return "Roar";
+	// }
+
+	public String sleep() {
+		return "rarr";
+	};
 }
 
 // polymorphic arguments & return types
 class Vet {
 	public static String giveShot(Animals a) {
-		return a.makeNoise();
+		return a.makeNoise() + " after shot, then " + a.sleep();
 	}
 }
 
-// main method
 class Main {
 	public static void main(String[] args) {
 		Animals[] animals = new Animals[3];
 		animals[0] = new Canine();
 		animals[1] = new Feline();
 		animals[2] = new Tiger();
+
+		// error: ArrayIndexOutOfBoundsException
+		// animals[3] = new Tiger();
+
+		// error: Animals is abstract; cannot be instantiated
 		// animals[3] = new Animals();
-		// prints 'none' if class is not abstract (note: array length)
-		// thows error if class is abstract
 
 		for ( int i = 0 ; i < animals.length ; i++ ) {
-			System.out.println(animals[i].makeNoise());
+			System.out.println(Vet.giveShot(animals[i]));
 		}
 	}
 }
+
+/*
+Prints:
+
+Woof after shot, then zzz
+Meow after shot, then purr
+Meow after shot, then rarr
+
+*/
