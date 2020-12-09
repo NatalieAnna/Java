f8f24172041d341575a552932a2f79a9a622ece0
Natalie <NatalieAnna@users.noreply.github.com>
2020-12-09 11:23:34 +1100

    Add files via upload

---
+++ Autoboxing/Main.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+
+class Main {
+	static void takeInteger(Integer i) {
+		System.out.println("takeInteger: " + i);
+	}
+
+	static void takeInt(int i) {
+		System.out.println("takeInt: " + i);
+	}
+
+	static int getInt(int x) {
+		return x;
+	}
+
+	static int getInteger(Integer x) {
+		return x;
+	}
+
+	public static void main(String [] args) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(7);
+		list.add(8);
+		list.add(9);
+
+		for ( int val : list ) {
+			System.out.println("Value: " + val);
+			takeInteger(val);
+			takeInt(val);
+			System.out.println("getInt: " + getInt(val));
+			System.out.println("getInteger: " + getInteger(val));
+		}
+
+		if (list.get(0) instanceof Integer) System.out.println("True");
+		else System.out.println("False");
+
+		// error: required: class or array
+		//if (list.get(0) instanceof int) System.out.println("True");
+		//else System.out.println("False");
+
+		// error: Integer cannot be converted to String
+		//if (list.get(0) instanceof String) System.out.println("True");
+		//else System.out.println("False");
+	}
+}
+
+/*
+Prints:
+
+Value: 7
+takeInteger: 7
+takeInt: 7
+getInt: 7
+getInteger: 7
+Value: 8
+takeInteger: 8
+takeInt: 8
+getInt: 8
+getInteger: 8
+Value: 9
+takeInteger: 9
+takeInt: 9
+getInt: 9
+getInteger: 9
+True
+
+*/
