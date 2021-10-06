ab6f6026d64b2c732708f2e5be9d2cb3c388dfcb
Natalie <NatalieAnna@users.noreply.github.com>
2021-10-07 10:40:29 +1100

    Add files via upload

---
+++ PackageAccess/PublicAccess/beverage/Wine.java
@@ -0,0 +1,15 @@
+// superclass Wine in package beverage
+package beverage;
+public abstract class Wine {
+	
+	public String colour;
+	public int alcohol;
+	
+	public String getColour() {
+		return colour;
+	}
+	
+	public int getAlcohol() {
+		return alcohol;
+	}
+}
