78684099afc65154600f130133086d354b8b2477
Natalie <NatalieAnna@users.noreply.github.com>
2021-10-07 10:58:06 +1100

    Add files via upload

---
+++ PackageAccess/FinalClass/Wine.java
@@ -0,0 +1,17 @@
+// superclass Wine in package beverage
+package beverage;
+public final class Wine {
+	
+	public String colour;
+	public int alcohol;
+	
+	public void importantMethod() {}
+	
+	public String getColour() {
+		return colour;
+	}
+	
+	public int getAlcohol() {
+		return alcohol;
+	}
+}
