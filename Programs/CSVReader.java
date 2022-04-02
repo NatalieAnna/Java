fb90f18cef0185f9be557e8e1fcbb3c4ce93b651
Natalie <NatalieAnna@users.noreply.github.com>
2022-04-03 09:32:51 +1000

    Update CSVReader.java
    Implements method.

--- Programs/CSVReader.java
+++ Programs/CSVReader.java
@@ -9,23 +9,28 @@ public class CSVReader {
 	public static void main(String[] args) {
 		String path = "uv-sydney-2020.csv";
 		String line = "";
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		LocalDateTime start = LocalDateTime.parse("2020-01-24 11:00:00", format);
+		LocalDateTime end = LocalDateTime.parse("2020-01-24 15:00:00", format);
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(path));
 			String skipHeaderLine = br.readLine();
-			DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-			LocalDateTime start = LocalDateTime.parse("2020-01-24 05:00:00", format);
-			LocalDateTime end = LocalDateTime.parse("2020-01-24 20:00:00", format);
+			System.out.println("Date:      Time:    UV:");
 			while ((line = br.readLine()) != null) {
-				String cols[] = line.split(","); // splits data into indexed columns
+				String[] cols = line.split(",");
 				LocalDateTime cell = LocalDateTime.parse(cols[0], format);
-				if (cell.isAfter(start) && cell.isBefore(end)) {
-					System.out.println(cols[0] + " " + cols[3]);
-				}
+				dataInDateRange(cell, cols, start, end);
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	// prints data in date range
+	public static void dataInDateRange(LocalDateTime cell, String[] cols, LocalDateTime start, LocalDateTime end) {
+                if (cell.isAfter(start) && cell.isBefore(end))
+			System.out.println(cols[0] + " " + cols[3]);
 	}
 }
