bdf0ab450c425f87b71a37278a394a633842c97d
Natalie <NatalieAnna@users.noreply.github.com>
2022-04-03 09:35:53 +1000

    Create Blockchain.java

---
+++ Programs/Blockchain/Blockchain.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class Blockchain {
+
+	public static LinkedList<Block> blockchain = new LinkedList<Block>();
+
+	public static void main(String[] args) {
+    // create genesis block
+		blockchain.add(new Block());
+		System.out.println(blockchain);
+	}
+}
