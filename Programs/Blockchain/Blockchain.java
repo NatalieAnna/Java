0328e168d5663f943be41b1c511cc80e442e119e
Natalie <NatalieAnna@users.noreply.github.com>
2022-04-06 06:42:51 +1000

    Update Blockchain.java

--- Programs/Blockchain/Blockchain.java
+++ Programs/Blockchain/Blockchain.java
@@ -3,16 +3,20 @@
  * @version April 2022
  */
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class Blockchain {
 
-	public static LinkedList<Block> blockchain = new LinkedList<Block>();
+	public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
 	public static void main(String[] args) {
-		
-    		// create genesis block
-		blockchain.add(new Block());
-		System.out.println(blockchain);
+
+		blockchain.add(new Block(Constants.GENESIS_PREV_HASH, Constants.GENESIS_DATA));
+		blockchain.add(new Block("second block"));
+		blockchain.add(new Block("third block"));
+
+		for (Block block : blockchain) {
+			System.out.println(block);
+		}
 	}
 }
